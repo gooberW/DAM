d3f1ebfc9809c27880fd258aa18a0dece315e093
Gabriel <gabrielrei2112@gmail.com>
2026-02-24 12:28:31 +0000

    Making calculator

--- DAM_TP1/src/main/kotlin/dam/exer_1/exer_1.kt
+++ DAM_TP1/src/main/kotlin/dam/exer_1/exer_1.kt
@@ -1,10 +1,28 @@
 package org.example.dam.exer_1
 
-fun main() {
-    val name = "Kotlin"
-    println("Hello, " + name + "!")
+import kotlin.collections.MutableList as MutableList
 
-    for (i in 1..5) { 
-        println("i = $i")
+fun main() {
+    val size = 51
+
+    // a)
+    var perfectSquaresA = IntArray(size){it*it}
+    perfectSquaresA = perfectSquaresA.sliceArray(1..size-1) // corta o 0
+    println("a) Perfect squares (first 50): ${perfectSquaresA.contentToString()}")
+
+    //b)
+    val numbersB: MutableList<Int> = mutableListOf()
+
+    for (i in 1..50) {
+        numbersB.add(i)
     }
+
+    val perfectSquaresB = numbersB.map {it*it}
+
+    println("b) Perfect squares (first 50): ${perfectSquaresB}")
+
+    //c)
+    var perfectSquaresC = Array<Int>(size) {it*it}
+    perfectSquaresC = perfectSquaresC.sliceArray(1..size-1) // corta o 0
+    println("c) Perfect squares (first 50): ${perfectSquaresC.contentToString()}")
 }
---
+++ DAM_TP1/src/main/kotlin/dam/exer_2/exer_2.kt
@@ -0,0 +1,109 @@
+package org.example.dam.exer_2
+
+
+fun main() {
+    var mode = selectMode()
+    while (!mode.equals("")) {
+
+        runCalc(mode)
+        mode = selectMode()
+    }
+}
+
+
+fun selectMode():Int {
+    println("Select calculator mode:")
+    println("1 - Arithmetic")
+    println("2 - Boolean")
+    println("3 - Bitwise Operations")
+
+    return readln().toInt()
+}
+
+fun runCalc(mode:Int) {
+    var result: Any = ""
+
+    when(mode) {
+        1 -> result = showArithmeticOp()
+        2 -> result = showBoolOp()
+        3 -> result = showShiftOp()
+    }
+
+
+
+
+    println("Result: ${result}")
+}
+
+fun showArithmeticOp():Any {
+    println("Type the first number:")
+    val num1 = readln().toFloat();
+    println("Type the second number:")
+    val num2 = readln().toFloat();
+
+    println("Select the intended operation typing the 3 letter code from the following list:")
+    println("ADD - Addition")
+    println("SUB - Subtraction")
+    println("MUL - Multiplication")
+    println("DIV - Division")
+
+
+
+    return handleArithmetic(num1, num2, readln())
+}
+
+fun showBoolOp(): Boolean {
+    println("Type the first value (true or false):")
+    val val1 = readln().toBoolean();
+    println("Type the second value (true or false):")
+    val val2 = readln().toBoolean();
+
+    print("Select the intended operation typing the 3 letter code from the following list:")
+    println("AND - Boolean AND")
+    println("OR - Boolean OR")
+    println("NOT - Boolean NOT")
+
+    return handleBoolean(val1, val2, readln())
+}
+
+fun showShiftOp():Any {
+    println("Type the number:")
+    val hexNum = readln().toInt();
+    println("Type the shift amount:")
+    val shift = readln().toInt();
+
+    println("SHL - Bitwise Shift Left")
+    println("SHR - Bitwise Shift Right")
+
+    return handleShift(hexNum, shift, readln())
+}
+
+fun handleArithmetic(num1:Float, num2:Float, operation: String): Any {
+    when(operation) {
+        "ADD" -> return num1 + num2
+        "SUB" -> return num1 - num2
+        "MUL" -> return num1 * num2
+        "DIV" -> if (num2 == 0f) return "ERR: Division by zero." else
+                                    return num1 / num2
+    }
+
+    return "No Result"
+}
+
+fun handleBoolean(val1: Boolean, val2: Boolean, operation: String): Boolean {
+    when(operation) {
+        "AND" -> return val1 && val2
+        "OR" -> return val1 || val2
+    }
+
+    return false
+}
+
+fun handleShift(hexNum: Int, shift: Int, operation: String):Any {
+    when(operation) {
+        "SHL" -> return (hexNum shl shift)
+        "SHR" -> return (hexNum shr shift)
+    }
+
+    return "No result"
+}
