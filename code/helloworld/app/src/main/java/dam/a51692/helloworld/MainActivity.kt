a0819ee39f235a9d8d2f1c5f0364721456a6663e
Gabriel <gabrielrei2112@gmail.com>
2026-03-07 15:15:34 +0000

    Added text input and title change (android app inprovements)

--- code/helloworld/app/src/main/java/dam/a51692/helloworld/MainActivity.kt
+++ code/helloworld/app/src/main/java/dam/a51692/helloworld/MainActivity.kt
@@ -1,10 +1,14 @@
 package dam.a51692.helloworld
 
 import android.os.Bundle
+import android.text.InputType
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,5 +23,26 @@ class MainActivity : AppCompatActivity() {
 
         println(getString(R.string.activity_oncreate_msg,
             this@MainActivity.localClassName))
+
+        val nameInput = findViewById<TextInputEditText>(R.id.nameInput)
+        val displayTextView = findViewById<TextView>(R.id.helloView)
+
+        nameInput.setOnEditorActionListener { input, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) { // se for a acaoo done do teclado
+                val userInput = input.text.toString()
+
+                if (userInput.isNotBlank()) {
+                    displayTextView.text = "Hello ${userInput}"
+                    nameInput.clearFocus()
+                }
+
+                // tem de retornar true para dizer que o evento foi handled
+                true
+            } else {
+                false
+            }
+        }
     }
+
+
 }
